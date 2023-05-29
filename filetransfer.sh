f32d0f8b63dcab4b56bb581f4765368d197ee71c
Cassie Jeng <59851290+cjeng8771@users.noreply.github.com>
2023-05-29 03:19:14 -0500

    readme and comments

--- filetransfer.sh
+++ filetransfer.sh
@@ -1,5 +1,5 @@
 
 for HOST in jengc@pc03-meb.emulab.net jengc@pc09-fort.emulab.net jengc@pc14-fort.emulab.net jengc@pc08-fort.emulab.net jengc@pc15-fort.emulab.net jengc@pc05-fort.emulab.net jengc@pc13-fort.emulab.net
 do
-    scp QPSK_signal_5-27-23.iq $HOST:
+    scp QPSK_signal_2023_01_18_neal.iq $HOST:
 done
